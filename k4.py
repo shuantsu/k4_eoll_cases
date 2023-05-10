c4552cf9169f132b605ec5f2394557cb76e5adf1
Filipe Teixeira <shuantsu@gmail.com>
2023-05-10 14:08:59 -0300

    updated script

--- k4.py
+++ k4.py
@@ -1,21 +1,19 @@
-import itertools
+import itertools, pprint
 
 combinations = list(itertools.product(range(2), repeat=8))
-
 legal_wings = lambda a: sum(a[1::2]) == sum(a[::2])
-
 result = list(filter(legal_wings, combinations))
 
 def get_symmetries(case):
     symmetries = []
-    i = list(case[:])
+    i = list(case)
     for n in range(4):
-        i.insert(0, i.pop())
-        i.insert(0, i.pop())
+        i = i[-2:] + i[:-2]
         symmetries.append(i[:])
     return str(sorted(symmetries))
 
 cases = map(get_symmetries, result)
 cases = list(map(eval, set([o[0] for o in [list(set([str(ii) for ii in eval(i)])) for i in cases]])))
 
-print(cases)
+pprint.pprint(cases)
+input()
